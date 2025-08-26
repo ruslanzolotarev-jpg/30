f310c65561c3d82531e94eec6e5105a80b99556f
ruslanzolotarev-jpg <666ruslanzolotarev@gmail.com>
2025-08-26 14:46:06 +0300

    Update fastapi.py

--- test/fastapi.py
+++ test/fastapi.py
@@ -1,8 +1,7 @@
-from ..app.main import app
 from fastapi.testclient import TestClient
+from app.main import app
 
 client = TestClient(app)
-
 
 def test_get_all_recipes():
     response = client.get("/recipes")
