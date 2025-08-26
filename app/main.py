c1afa35f6f8aff298eb3cc13734932f01372a6cf
ruslanzolotarev-jpg <666ruslanzolotarev@gmail.com>
2025-08-26 14:30:44 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -33,5 +33,3 @@ async def get_popular_recipes():
          response_model=Union[schemas.RecipeOutSecond, str])
 async def get_recipe(rec_id: int = Path(...)):  # noqa: B008
     return await get_recipe_by_id(rec_id)
-
-
